ab97393a60a88d0bc7fc4902066ec78bc656c973
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2022-12-28 19:17:47 -0300

    First test: return same input number

---
+++ FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzCalculator
+    {
+        public static int Check(int number)
+        {
+            return number;
+        }
+    }
+}
--- FizzBuzzKata/UnitTest1.cs
+++ FizzBuzzKata/UnitTest1.cs
@@ -1,11 +1,36 @@
+using FizzBuzz;
+
 namespace FizzBuzzKata
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestReturnSameNumber()
         {
+            #region -- Arrange --
+            int numberNotDivisibleBy3Nor5 = GetNumberNotDivisibleBy3Nor5();
+            #endregion
 
+            #region -- Act --
+            int result = FizzBuzzCalculator.Check(numberNotDivisibleBy3Nor5);
+            #endregion
+
+            #region -- Assert --
+            Assert.Equal(numberNotDivisibleBy3Nor5, result);
+            #endregion
+        }
+
+        private int GetNumberNotDivisibleBy3Nor5()
+        {
+            Random random = new Random();
+            int randomNumber;
+
+            do
+            {
+                randomNumber = random.Next(1, int.MaxValue);
+            } while (randomNumber % 3 == 0 && randomNumber % 5 == 0);
+
+            return randomNumber;
         }
     }
 }
